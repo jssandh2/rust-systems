260b41f3063631f9bd48817d221aaf4bd2acb2d7
Juspreet Sandhu <juspreet@jssandh2@gmail.com>
2017-09-19 00:58:21 -0400

    Build the project folder using Cargo --> cargo build

---
+++ target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/juspreet/Desktop/Github_Code/rust-systems/target/debug/hello_world: /home/juspreet/Desktop/Github_Code/rust-systems/src/test/hello_world.rs
--- test/hello_world.rs
+++
@@ -1,6 +0,0 @@
-// Test to make sure :
-// 1) The Compiler works -- rustc hello_world.rs
-// 2) Basic main function syntax with I/O
-fn main() {
-   println!("Hello, World!");
-}
